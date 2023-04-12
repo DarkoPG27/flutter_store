8e26e8d5233c67ad6f1a012ac447fa87821ce182
DarkoPG27 <darkobrnovic27@gmail.com>
2023-04-12 15:13:29 +0200

    Listening in Different Places and Ways

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   /*  final String title;
@@ -12,10 +15,12 @@ class ProductDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final productId =
         ModalRoute.of(context).settings.arguments as String; //is the id!
-    //...
+    final loadProduct =
+        Provider.of<Products>(context, listen: false).findById(productId);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("title"),
+        title: Text(loadProduct.title),
       ),
     );
   }
