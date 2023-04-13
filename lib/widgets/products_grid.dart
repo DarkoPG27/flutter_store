d9f4ec01064b5d5a1f3bc5b340e6008ec3b98309
DarkoPG27 <darkobrnovic27@gmail.com>
2023-04-13 08:42:45 +0200

    Alternative Provider Syntaxes

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -14,8 +14,9 @@ class ProductsGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10),
       itemCount: products.length,
-      itemBuilder: (ctx, index) => ChangeNotifierProvider(
-        create: (c) => products[index],
+      itemBuilder: (ctx, index) => ChangeNotifierProvider.value(
+        value: products[index],
+        /*   create: (c) => products[index], */
         child: ProductItem(
             /*  products[index].id,
           products[index].title,
