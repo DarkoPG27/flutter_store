724c7258d3895d0c679c7a67c40df5d96f886a07
DarkoPG27 <darkobrnovic27@gmail.com>
2023-04-13 09:05:32 +0200

    enum FilterOptions

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -2,12 +2,37 @@ import 'package:flutter/material.dart';
 
 import '../widgets/products_grid.dart';
 
+enum FilterOptions {
+  Favorites,
+  All,
+}
+
 class ProductsOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              print(selectedValue);
+            },
+            icon: Icon(
+              Icons.more_vert,
+            ),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only favorites'),
+                value: FilterOptions.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text('Show all'),
+                value: FilterOptions.All,
+              )
+            ],
+          )
+        ],
       ),
       body: ProductsGrid(),
     );
