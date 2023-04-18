43565efcb0fc4f8fb279d499475e000fcc907365
DarkoPG27 <darkobrnovic27@gmail.com>
2023-04-18 10:03:04 +0200

    Using Forms and Working with Form Inputs

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/screens/edit_product_screen.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/products.dart';
 
 import '../widgets/user_product_item.dart';
 import '../widgets/app_drawer.dart';
+
+import '../screens/edit_product_screen.dart';
 
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
@@ -19,7 +22,7 @@ class UserProductsScreen extends StatelessWidget {
           IconButton(
             icon: const Icon(Icons.add),
             onPressed: () {
-              //...
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
             },
           )
         ],
