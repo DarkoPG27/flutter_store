e6672430acd14c938349ddb9c4d80bb3c6cf2e74
DarkoPG27 <darkobrnovic27@gmail.com>
2023-04-20 08:30:30 +0200

    Sending POST Requests

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
 
 import './product.dart';
 
@@ -51,6 +55,17 @@ class Products with ChangeNotifier {
   }
 
   void addProduct(Product product) {
+    const url =
+        'https://flutter-store-b70a4-default-rtdb.firebaseio.com/products.json';
+    http.post(url,
+        body: json.encode({
+          'title': product.title,
+          'description': product.description,
+          'price': product.price,
+          'imageUrl': product.imageUrl,
+          'isFavorite': product.isFavorite,
+        }));
+
     final newProduct = Product(
       title: product.title,
       description: product.description,
