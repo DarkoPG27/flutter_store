ce4040076072844e924e8507753c9abb47d7e071
DarkoPG27 <darkobrnovic27@gmail.com>
2023-04-20 12:58:55 +0200

    Utilizing Optimistic Updating

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -128,7 +128,19 @@ class Products with ChangeNotifier {
   }
 
   void deleteProduct(String id) {
-    _items.removeWhere((prod) => prod.id == id);
+    final url =
+        'https://flutter-store-b70a4-default-rtdb.firebaseio.com/products/$id.json';
+    final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
+    var existingProduct = _items[existingProductIndex];
+
+    http.delete(url).then((response) {
+      if (response.statusCode >= 400) {}
+      existingProduct = null;
+    }).catchError((_) {
+      _items.insert(existingProductIndex, existingProduct);
+      notifyListeners();
+    });
+    _items.removeAt(existingProductIndex);
     notifyListeners();
   }
 }
