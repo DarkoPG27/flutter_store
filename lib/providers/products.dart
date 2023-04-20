f7d76ba9d76ff66f2386d574db2b8cb1ea78b683
DarkoPG27 <darkobrnovic27@gmail.com>
2023-04-20 08:41:51 +0200

    Working with Futures in Dart

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -57,25 +57,29 @@ class Products with ChangeNotifier {
   void addProduct(Product product) {
     const url =
         'https://flutter-store-b70a4-default-rtdb.firebaseio.com/products.json';
-    http.post(url,
-        body: json.encode({
-          'title': product.title,
-          'description': product.description,
-          'price': product.price,
-          'imageUrl': product.imageUrl,
-          'isFavorite': product.isFavorite,
-        }));
-
-    final newProduct = Product(
-      title: product.title,
-      description: product.description,
-      price: product.price,
-      imageUrl: product.imageUrl,
-      id: DateTime.now().toString(),
-    );
-    _items.add(newProduct);
+    http
+        .post(
+      url,
+      body: json.encode({
+        'title': product.title,
+        'description': product.description,
+        'price': product.price,
+        'imageUrl': product.imageUrl,
+        'isFavorite': product.isFavorite,
+      }),
+    )
+        .then((response) {
+      print(json.decode(response.body));
+      final newProduct = Product(
+          title: product.title,
+          description: product.description,
+          price: product.price,
+          imageUrl: product.imageUrl,
+          id: json.decode(response.body)['name']);
+      _items.add(newProduct);
 /*     _items.insert(0, newProduct); //at the start of the list */
-    notifyListeners();
+      notifyListeners();
+    });
   }
 
   void updateProduct(String id, Product newProduct) {
